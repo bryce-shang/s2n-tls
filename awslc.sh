a98b9400f46ba73380388ab87ec3f883346a6347
Bryce Shang <shanggu@amazon.com>
2020-12-10 21:14:49 -0800

    correct script.

--- awslc.sh
+++ awslc.sh
@@ -1,9 +1,12 @@
 set -ex
 
+# https://github.com/awslabs/s2n/blob/main/docs/USAGE-GUIDE.md
+export S2N_PRINT_STACKTRACE=1
 export CB_BIN_DIR="./codebuild/bin"
 export S2N_LIBCRYPTO="awslc"
-export TESTS="integration"
 export BUILD_S2N=true
 
+TESTS=integration
 GCC_VERSION=9
+$CB_BIN_DIR/install_default_dependencies.sh
 $CB_BIN_DIR/s2n_codebuild.sh
