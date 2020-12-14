ff705faa75b669b9fc9573baf902c2ef0f49263e
Bryce Shang <shanggu@amazon.com>
2020-12-13 22:06:22 -0800

    remove build file.

--- awslc.sh
+++ awslc.sh
@@ -6,6 +6,7 @@ export CB_BIN_DIR="./codebuild/bin"
 export S2N_LIBCRYPTO="awslc"
 export BUILD_S2N=true
 
+rm nohup.out
 TESTS=integration
 GCC_VERSION=9
 # $CB_BIN_DIR/install_default_dependencies.sh
